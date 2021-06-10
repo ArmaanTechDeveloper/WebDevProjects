60e0c2f63f9b3678cebd82e6502ab48c6c26776d
ArmaanTechDeveloper <kournarinder948@gmail.com>
2021-06-10 14:45:50 +0530

    added js file and modified the css of tab section mainly

---
+++ FrontEnd/Velocity Website/JS/index.js
@@ -0,0 +1,11 @@
+document.getElementById('buttonOne').onclick = function(){
+    document.getElementById('changeImg').src = "https://assets.website-files.com/5e4b18feebfd1a721bb930e6/5e4b18feebfd1a316cb93127_photo-1422222948315-28aadb7a2cb8-p-1080x721.jpeg";
+}
+document.getElementById('buttonTwo').onclick = function(){
+    document.getElementById('changeImg').src = "https://assets.website-files.com/5e4b18feebfd1a721bb930e6/5e4b18feebfd1ab3f1b93110_5fcb0a55.jpg";
+}
+document.getElementById('buttonThree').onclick = function(){
+    document.getElementById('changeImg').src = "https://assets.website-files.com/5e4b18feebfd1a721bb930e6/5e4b18feebfd1a4509b93139_5cf8b62b-p-1080x718.jpeg";
+}
+
+
